9cac13a50a64b9f8b871252309a19f0954cce1b8
michal92299 <chudybyczek2137@gmail.com>
2026-09-03 15:25:18 +0200

    Update fasttree.nim

--- src/fasttree.nim
+++ src/fasttree.nim
@@ -1,1 +1,12 @@
+runnableExamples:
+  discard
 
+import fasttree/hashing
+import fasttree/chunker
+import fasttree/store
+import fasttree/manifest
+import fasttree/ioengine
+import fasttree/oci
+import fasttree/composefs
+
+export hashing, chunker, store, manifest, ioengine, oci, composefs
